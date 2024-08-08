8d33e346fd5c0291131fcfd5b3e7a3c931f9366e
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-08-07 18:27:09 -0600

    weather startings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 const cloudy = document.getElementsByClassName('cloudy-playlist')
 
+const apiKey = '7a0fbe2742fb17dd43551428d82f00d1';
+const city = 'London'; // Specify the city for which you want to fetch weather data
 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Display the weather data in the console
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
